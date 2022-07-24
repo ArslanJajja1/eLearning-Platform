230b08268c2b0707ce128a17898e7c93fa2a0692
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-24 14:51:18 +0500

    mongodb setup,routes,controllers setup

---
+++ server/controllers/auth.js
@@ -0,0 +1,3 @@
+exports.register = (req, res) => {
+    res.send("Wellcome to our application ");
+};
--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import { readdirSync } from "fs";
 const morgan = require("morgan");
 require("dotenv").config();
-
+//database connection
+mongoose
+    .connect(process.env.DATABASE)
+    .then((res) => console.log("Database connection successfull"))
+    .catch((error) =>
+        console.log("Database connection failed" + error.message)
+    );
+// Middlewares
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -11,13 +20,11 @@ app.use((req, res, next) => {
     console.log("This is my own middleware...");
     next();
 });
-
-app.get("/", (req, res) => {
-    res.send("Wellcome to our application ");
-});
-
+//routes
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
+//port
 const port = process.env.PORT || 8000;
-
+//listen
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
