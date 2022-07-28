f3b386b1fd27453768e462789b8ffe2b7895f779
ArslanJajja1 <arslanjajja13@gmail.com>
2022-07-28 16:53:32 +0500

    handling expired token using axios interceptors

--- client/context/index.js
+++ client/context/index.js
@@ -1,5 +1,6 @@
 import { useReducer, createContext, useEffect } from "react";
-
+import axios from "axios";
+import { useRouter } from "next/router";
 // Initial State
 const initialState = {
     user: null,
@@ -23,12 +24,47 @@ const rootReducer = (state, action) => {
 
 const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(rootReducer, initialState);
+    const router = useRouter();
     useEffect(() => {
         dispatch({
             type: "LOGIN",
             payload: JSON.parse(window.localStorage.getItem("user")),
         });
     }, []);
+    axios.interceptors.response.use(
+        (response) => {
+            // any status code that lie in range 2xx cause this function to trigger
+            return response;
+        },
+        (error) => {
+            // any status code that lie in range out of 2xx cause this function to trigger
+            let res = error.reponse;
+            if (
+                res.status === 401 &&
+                res.config &&
+                !res.config.__isRetryRequest
+            ) {
+                return new Promise((resolve, reject) => {
+                    axios
+                        .get("/api/logout")
+                        .then((data) => {
+                            console.log("res data", data);
+                            console.log("401 error - logout");
+                            dispatch({
+                                type: "LOGOUT",
+                            });
+                            window.localStorage.removeItem("user");
+                            router.push("/login");
+                        })
+                        .catch((error) => {
+                            reject(error);
+                            console.log(error);
+                        });
+                });
+            }
+            return Promise.reject(error);
+        }
+    );
     return (
         <Context.Provider value={{ state, dispatch }}>
             {children}
